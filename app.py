cf48fb9cb015feaf5aca48f79e894cebbdba0e1c
Aachal Kala <66769063+Aachal622@users.noreply.github.com>
2021-05-17 15:18:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+%%writefile app.py
+import streamlit as st 
+from PIL import Image
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+st.set_option('deprecation.showfileUploaderEncoding', False)
+# Load the pickled model
+model = pickle.load(open('decision_model.pkl', 'rb')) 
+model_decision_tree = pickle.load(open('decision_model.pkl', 'rb')) 
+dataset= pd.read_csv('Classification Dataset1.csv')
+X = dataset.iloc[:, [2, 3]].values
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+# def predict_note_au be purchased:
+#   print(prediction)
+#   return prediction
+def predict_random(UserID, Gender,Age,EstimatedSalary):
+  output= model_randomforest.predict(sc.transform([[Age,EstimatedSalary]]))
+  print("Purchased", output)
+  if output==[1]:
+    prediction="Item will be purchased"
+  else:
+    prediction="Item will not be purchased"
+  print(prediction)
+  return prediction
+def main():
+    
+    html_temp = """
+   <div class="" style="background-color:blue;" >
+   <div class="clearfix">           
+   <div class="col-md-12">
+   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
+   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
+   <center><p style="font-size:25px;color:white;margin-top:10px;"Machine Learning Lab Experiment</p></center> 
+   </div>
+   </div>
+   </div>
+   """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    st.header("Item Purchase Prediction using decision tree Algorithm")
+    UserID = st.text_input("UserID","Type Here")
+    Gender = st.selectbox(
+    "Gender",
+    ("Male", "Female", "Others")
+    )
+    
+    Age = st.number_input('Insert a Age',0,100)
+    #Age = st.text_input("Age","Type Here")
+    EstimatedSalary = st.number_input("Insert EstimatedSalary",1000,1000000000)
+    resul=""
+    if st.button("Prediction"):
+      result=predict_note_authentication(UserID, Gender,Age,EstimatedSalary)
+      st.success('Model has predicted {}'.format(result))
+    if st.button("Decision tree Prediction"):
+      result=predict_random(UserID, Gender,Age,EstimatedSalary)
+      st.success('Decision tree Model  has predicted {}'.format(result))  
+    if st.button("About"):
+      st.header("Developed by Aachal Kala")
+      st.subheader("Student , Department of Computer Engineering")
+    html_temp = """
+    <div class="" style="background-color:orange;" >
+    <div class="clearfix">           
+    <div class="col-md-12">
+    <center><p style="font-size:20px;color:white;margin-top:10px;">Machine Learning Experiment 5: Support Vector Machine and Random Forest</p></center> 
+    </div>
+    </div>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+if __name__=='__main__':
+  main()
